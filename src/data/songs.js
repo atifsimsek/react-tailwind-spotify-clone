42c55c0bccc9a5939a85b47754de9e51daf3fd7a
Atıf Şimşek <dev.atifsimsek@gmail.com>
2022-12-13 12:34:07 +0300

    project finished

---
+++ src/data/songs.js
@@ -0,0 +1,48 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+    {
+        id: 1,
+        title: "Abbey Road",
+        description: "The Beatles",
+        image: "https://i.scdn.co/image/ab67616d00001e02dc30583ba717007b00cceb25",
+        type: "album",
+        artist: "Artist 1",
+        src: 'https://freesound.org/data/previews/612/612095_5674468-lq.mp3'
+    },
+    {
+        id: 2,
+        title: "Evolve",
+        description: "İmagine Dragons",
+        image: "https://i.scdn.co/image/ab67616d00001e025675e83f707f1d7271e5cf8a",
+        type: "album",
+        artist: "Artist 2",
+        src: 'https://freesound.org/data/previews/612/612092_7037-lq.mp3'
+    },
+    {
+        id: 3,
+        title: "Artist",
+        description: "Pink Floyd, Metallica, Ghost ve daha fazlası",
+        image: "https://i.scdn.co/image/d011c95081cd9a329e506abd7ded47535d524a07",
+        type: "artist",
+        artist: "Artist 3",
+        src: 'https://freesound.org/data/previews/612/612087_1648170-lq.mp3'
+    },
+    {
+        id: 4,
+        title: "PodCast",
+        description: "Imagine Dragons, Coldplay, The Score ve daha fazlası",
+        image: "https://i.scdn.co/image/ab67656300005f1fa5dee41b5350c78b3887f56f",
+        type: "podcast",
+        artist: "Artist 4",
+        src: 'https://freesound.org/data/previews/612/612085_28867-lq.mp3'
+    },
+    {
+        id: 5,
+        title: "Album",
+        description: "The Score",
+        image: "https://i.scdn.co/image/ab67616d00001e0284f08a509689b140f1bf931d",
+        type: "album",
+        artist: "Artist 5",
+        src: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3'
+    }
+]
